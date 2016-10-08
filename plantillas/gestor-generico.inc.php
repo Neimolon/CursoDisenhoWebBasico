848f074fe6a18896e4a264c90ed8dce15e2b5696
Neimolon <diegomatos1983@gmail.com>
2016-10-08 20:29:56 +0200

    42 - Gestor Blog

--- plantillas/gestor-generico.inc.php
+++ plantillas/gestor-generico.inc.php
@@ -1,1 +1,42 @@
-gestor generico
+<div class="row text-center">
+    <div id="gg-entradas" class="col-md-4 gg-elemento">
+        <h2><i class="fa fa-newspaper-o" aria-hidden="true"></i></h2>
+        <h3>Entradas</h3>
+        <hr>
+        <h4>129</h4>
+        <h5>Entradas publicadas</h5>
+        <br>
+        <h4>24</h4>
+        <h5>Borradores</h5>
+        <br>
+        <h4>1003</h4>
+        <h5>Likes en articulos</h5>
+
+    </div>
+    <div id="gg-comentarios" class="col-md-4 gg-elemento">
+        <h2><i class="fa fa-comments" aria-hidden="true"></i></h2>
+        <h3>Comentarios</h3>
+        <hr>
+        <h4>129</h4>
+        <h5>Comentarios escritos</h5>
+        <br>
+        <h4>24</h4>
+        <h5>Comentarios eliminados</h5>
+        <br>
+        <h4>1003</h4>
+        <h5>Comentarios en artículos</h5>
+    </div>
+    <div id="gg-favoritos" class="col-md-4 gg-elemento">
+        <h2><i class="fa fa-star" aria-hidden="true"></i></h2>
+        <h3>Favoritos</h3>
+        <hr>
+        <h4>129</h4>
+        <h5>Entradas favoritos</h5>
+        <br>
+        <h4>24</h4>
+        <h5>Autores molones</h5>
+        <br>
+        <h4>1003</h4>
+        <h5>Usuarios favoritos</h5>
+    </div>
+</div>
